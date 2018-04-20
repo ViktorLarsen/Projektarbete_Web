cdb2b80c69a9fe8ba21116b19a9a15663e0de18a
Viktor Larsen <32090557+ViktorLarsen@users.noreply.github.com>
2018-04-20 11:20:58 +0200

    Add IsRecruiter checkbox to register form.

--- Vrektproject/Models/AccountViewModels/RegisterViewModel.cs
+++ Vrektproject/Models/AccountViewModels/RegisterViewModel.cs
@@ -31,5 +31,7 @@ namespace Vrektproject.Models.AccountViewModels
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        public bool IsRecruiter { get; set; }
     }
 }
