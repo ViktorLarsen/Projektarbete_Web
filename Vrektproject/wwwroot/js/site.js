eabb598036faca7b622d021ae6483d18538ddd17
Unknown <emilski_1@hotmail.com>
2018-05-18 12:41:39 +0200

    heheXD

--- Vrektproject/wwwroot/js/site.js
+++ Vrektproject/wwwroot/js/site.js
@@ -1,69 +1,76 @@
 ﻿//import { triggerAsyncId } from "async_hooks";
 
 // Write your JavaScript code.
-//document.addEventListener('load', function (event) {
+window.addEventListener('load', function (event){
 
-let btn = document.getElementById('mysteryButton');
-let likeBtn = document.getElementById('likeBtn');
-let div = document.getElementById('mysteryDiv');
-
-let tinderButton = document.getElementById('tinderButton');
-
+//let btn = document.getElementById('mysteryButton');
+//let div = document.getElementById('mysteryDiv');
+    if (document.getElementById('likeBtn') !== null) {
+        let likeBtn = document.getElementById('likeBtn');
+    }
+    if (document.getElementById('tinderButton') !== null) {
+        let tinderButton = document.getElementById('tinderButton');
+    }
 let profileTemplate = document.getElementById('profileTemplate');
 let descriptionTemplate = document.getElementById('descriptionTemplate');
 let imageTemplate = document.getElementById("imageTemplate");
-
 let secretId = document.getElementById('secretId');
 let secretProfileId = document.getElementById('secretProfileId');
 let counter = 0;
 
-tinderButton.addEventListener('click', function (event) {
-    fetch('/Api/Finder/GetProfiles?id=' + secretId.textContent)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            try {
-                var profile = JSON.parse(data[counter]);
-                counter++;
+    if (document.getElementById('tinderButton') !== null) {
+        tinderButton.addEventListener('click', function (event) {
+            fetch('/Api/Finder/GetProfiles?id=' + secretId.textContent)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    try {
+                        var profile = JSON.parse(data[counter]);
+                        counter++;
 
-                nameTemplate.innerHTML = profile.FirstName + ' ' + profile.LastName;
-                descriptionTemplate.innerHTML = profile.Description;
-                imageTemplate.src = "data:image/png;base64," + profile.AvatarImage;
+                        nameTemplate.innerHTML = profile.FirstName + ' ' + profile.LastName;
+                        descriptionTemplate.innerHTML = profile.Description;
+                        imageTemplate.src = "data:image/png;base64," + profile.AvatarImage;
 
-                secretProfileId.innerHTML = profile.UserId;
-                likeBtn.className = 'btn btn-success pull-right';
-                tinderButton.textContent = 'Keep looking';
-                tinderButton.className = 'btn pull-left';
-                console.log("API run successfully");
+                        secretProfileId.innerHTML = profile.UserId;
+                        likeBtn.className = 'btn btn-success pull-right';
+                        tinderButton.textContent = 'Keep looking';
+                        tinderButton.className = 'btn pull-left';
+                        console.log("API run successfully");
 
-            }
-            catch (e) {
-                nameTemplate.innerHTML = 'No more profiles!';
-                descriptionTemplate.innerHTML = '';
-                imageTemplate.src = '';
-                likeBtn.className = 'btn btn-success hidden';
-                tinderButton.className = 'btn hidden';
-                console.log("No more profiles (or failed to parse profile object)");
-            }
-        })
-        .catch(error => {
-            console.log(error);
+                    }
+                    catch (e) {
+                        nameTemplate.innerHTML = 'No more profiles!';
+                        descriptionTemplate.innerHTML = '';
+                        imageTemplate.src = '';
+                        likeBtn.className = 'btn btn-success hidden';
+                        tinderButton.className = 'btn hidden';
+                        console.log("No more profiles (or failed to parse profile object)");
+                        counter = 0;
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         });
-});
+    }
 
+    if (document.getElementById('likeBtn') !== null) {
 likeBtn.addEventListener('click', function (event) {
-    let url = '/Api/Finder/Like?id=' + secretId.textContent + '&likeId=' + secretProfileId.textContent
-    console.log(url)
+    let url = '/Api/Finder/Like?id=' + secretId.textContent + '&likeId=' + secretProfileId.textContent;
+    console.log(url);
     fetch(url)
         .then(response => {
             return response.json();
         })
         .then(data => {
             try {
+                if (counter > 0) {
+                    counter--;
+                }
                 var profile = JSON.parse(data[counter]);
-                counter++;
-
+                
                 nameTemplate.innerHTML = profile.FirstName + ' ' + profile.LastName;
                 descriptionTemplate.innerHTML = profile.Description;
                 imageTemplate.src = "data:image/png;base64," + profile.AvatarImage;
@@ -73,33 +80,39 @@ likeBtn.addEventListener('click', function (event) {
                 tinderButton.textContent = 'Keep looking';
                 tinderButton.className = 'btn pull-left';
                 console.log("API run successfully");
+                
 
             }
             catch (e) {
+                var arrayLength = data.length
                 nameTemplate.innerHTML = 'No more profiles!';
                 descriptionTemplate.innerHTML = '';
                 imageTemplate.src = '';
                 likeBtn.className = 'btn btn-success hidden';
                 tinderButton.className = 'btn hidden';
                 console.log("No more profiles (or failed to parse profile object)");
+                counter = 0;
+                console.log(arrayLength);
+                console.log(counter);
             }
         })
         .catch(error => {
             console.log(error);
         });
-});
-
-btn.addEventListener('click', function (event) {
-    fetch('/Api/MysteryAnimal')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            div.innerHTML += '<br/>' + data;
-        })
-        .catch(error => {
-            console.log(error);
         });
-});
+    }
+    });
 
+
+//btn.addEventListener('click', function (event) {
+//    fetch('/Api/MysteryAnimal')
+//        .then(response => {
+//            return response.json();
+//        })
+//        .then(data => {
+//            div.innerHTML += '<br/>' + data;
+//        })
+//        .catch(error => {
+//            console.log(error);
+//        });
 //});
